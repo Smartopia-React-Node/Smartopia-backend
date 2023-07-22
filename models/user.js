da529edd5c01e6bf2d493cd358557637d8232eb5
Soumik234 <115613165+Soumik234@users.noreply.github.com>
2023-07-22 13:33:41 +0530

    customCollection

--- models/user.js
+++ models/user.js
@@ -1,48 +1,90 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     fname: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     lname: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     phoneNum: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    mainAdmin:{
-        type: Boolean,
-        default: false
+    mainAdmin: {
+      type: Boolean,
+      default: false,
     },
     email: {
-        type: String,
-        required: true,
-        max: 50,
-        unique: true,
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    saveProduct:{
-        type: Array,
-        default: []
+    saveProduct: {
+      type: Array,
+      default: [],
     },
-    likedProduct:{
-        type: Array,
-        default: []
+    likedProduct: {
+      type: Array,
+      default: [],
     },
-    type:{
-        type: String,
-        default: ""
-    }
-},  { timestamps: true })
+    type: {
+      type: String,
+      default: "",
+    },
+    // customTool: {
+    //   type: [
+    //     {
+    //       toolId: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Tool",
+    //         required: true,
+    //       },
+    //       fname: {
+    //         type: String,
+    //         required: true,
+    //       },
+    //       toolName: {
+    //         type: String,
+    //         required: true,
+    //       },
+    //       priceModel: {
+    //         type: String,
+    //         required: true,
+    //       },
+    //     },
+    //   ],
+    //   default: [],
+    // },
+    customCollection: {
+      type: [
+        {
+          collectionName: {
+            type: String,
+            required: true,
+            maxlength: 50,
+          },
+          saveTools: {
+            type: Array,
+            default: [],
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
 const User = require("../models/user")
-
+const Tool = require("../models/tool");
 
 //ALL User
 router.get("/all", async (req, res)=>{
@@ -129,6 +130,132 @@ router.get("/like/all/:id", async(req, res) => {
    }
 });
 
+
+// // GET all custom tools for a user
+// router.get("/:id/customTools", async (req, res) => {
+//   try {
+//     const userId = req.params.id;
+//     const user = await User.findById(userId).populate("customTool.toolId", "fname toolName priceModel");
+//     if (!user) {
+//       return res.json([]); 
+//     }
+//     const customTools = user.customTool.map((customTool) => {
+//       return {
+//         id: customTool.toolId._id.toString(),
+//         fname: customTool.fname,
+//         toolName: customTool.toolId.toolName,
+//         priceModel: customTool.toolId.priceModel,
+//       };
+//     });
+//     res.json(customTools);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json({ error: err.message });
+//   }
+// });
+
+
+
+// // POST a new custom tool for a user
+// router.post("/:id/customTools", async (req, res) => {
+//   try {
+//     const userId = req.params.id;
+//     const newCustomTool = {
+//       fname: req.body.fname,
+//       toolName: req.body.toolName,
+//       priceModel: req.body.priceModel,
+//     };
+
+//     const createdTool = await Tool.create(newCustomTool);
+
+//     const customTool = {
+//       toolId: createdTool._id,
+//       fname: req.body.fname,            // Include the fname field
+//       toolName: req.body.toolName,
+//       priceModel: req.body.priceModel,
+//     };
+
+//     const updatedUser = await User.findByIdAndUpdate(
+//       userId,
+//       { $push: { customTool: customTool } },
+//       { new: true }
+//     );
+
+//     const updatedCustomTools = updatedUser.customTool.map((customTool) => {
+//       return {
+//         id: customTool.toolId.toString(),
+//         fname: customTool.fname,         // Include the fname field
+//         toolName: customTool.toolName,
+//         priceModel: customTool.priceModel,
+//       };
+//     });
+
+//     res.status(201).json(updatedCustomTools);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).json({ error: "Failed to create custom tool" });
+//   }
+// });
+
+
+
+// // DELETE a custom tool for a user
+// router.delete("/customTools/:id", (req, res) => {
+//   const userId = req.params.id;
+//   const customToolId = req.body.customToolId; 
+//   User.findByIdAndUpdate(
+//     userId,
+//     { $pull: { customTool: { _id: customToolId } } }, 
+//     { new: true }
+//   )
+//     .then((updatedUser) => {
+//       res.json(updatedUser.customTool);
+//     })
+//     .catch((err) => res.status(400).json({ error: err.message }));
+// });
+
+// GET customCollection by collectionName
+router.get("/customCollections/:collectionName", async (req, res) => {
+  const { collectionName } = req.params;
+
+  try {
+    const user = await User.findOne({ "customCollection.collectionName": collectionName });
+    if (!user) {
+      return res.status(404).json({ message: "Custom collection not found" });
+    }
+    
+    const customCollection = user.customCollection.find(item => item.collectionName === collectionName);
+    return res.status(200).json(customCollection);
+  } catch (err) {
+    console.error("Error fetching custom collection:", err);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.post("/customCollections/:userId", async (req, res) => {
+  const { collectionName, saveTools } = req.body;
+  const { userId } = req.params;
+
+  try {
+    const existingCollection = await User.findOne({ _id: userId, "customCollection.collectionName": collectionName });
+    if (existingCollection) {
+      return res.status(400).json({ message: "Collection with this name already exists for the user" });
+    }
+    const newCustomCollection = { collectionName, saveTools };
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.customCollection.push(newCustomCollection);
+    await user.save();
+
+    return res.status(201).json(newCustomCollection);
+  } catch (err) {
+    console.error("Error creating custom collection:", err);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
 
 
 //  //TOOL LIKE
